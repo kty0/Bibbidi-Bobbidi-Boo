e1667e010dd09f37c734f2c5074d4fc4ff500139
Cathy Nguyen <cathy.nguyen.75013@gmail.com>
2024-09-07 14:22:07 +0200

    fix: debug cursor change when extension is activate

--- public/content.js
+++ public/content.js
@@ -9,20 +9,26 @@ function getRandomColor() {
   return color;
 }
 
+function changeCursor() {
+  selectMode
+    ? (document.body.style.cursor = "crosshair")
+    : (document.body.style.cursor = "default");
+}
+
 chrome.runtime.sendMessage({ action: "getLocalStorage" }, (response) => {
   console.log("Content : ", response);
   selectMode = response.selectMode;
+  changeCursor();
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request);
   if (request.action === "activate") {
     selectMode = true;
-    document.body.style.cursor = "crosshair";
   } else if (request.action === "desactivate") {
     selectMode = false;
-    document.body.style.cursor = "default";
   }
+  changeCursor();
 });
 
 document.addEventListener("click", (elem) => {
