beb05135a076d6b272004084c5d6a8a5abb89e9e
Cathy Nguyen <cathy.nguyen.75013@gmail.com>
2024-09-07 15:46:59 +0200

    enhancement: add magic wand and magic dust

--- public/content.js
+++ public/content.js
@@ -9,10 +9,12 @@ function getRandomColor() {
   return color;
 }
 
+console.log(chrome.runtime.getURL("magic-wand-cursor.png"));
+
 function changeCursor() {
-  selectMode
-    ? (document.body.style.cursor = "crosshair")
-    : (document.body.style.cursor = "default");
+  document.body.style.cursor = selectMode
+    ? `url('${chrome.runtime.getURL("magic-wand-cursor.png")}'), crosshair`
+    : "default";
 }
 
 chrome.runtime.sendMessage({ action: "getLocalStorage" }, (response) => {
@@ -40,3 +42,62 @@ document.addEventListener("click", (elem) => {
     }
   }
 });
+
+// //add style for sparkles
+
+// var style = document.createElement("style");
+// style.innerHTML = `
+// .star {
+//   position: fixed;
+//   pointer-events: none;
+// }
+
+// .star:before,
+// .star:after {
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   content: "\\2726";
+//   font-size: 9px;
+// }
+
+// .star:before {
+//   color: transparent;
+//   text-shadow: 0 0 3px rgb(250, 250, 174);
+// }
+
+// .star:after {
+//   background: #A5D8FF;
+//   -webkit-background-clip: text;
+//   -webkit-text-fill-color: transparent;
+// }
+// `;
+
+// document.head.appendChild(style);
+
+// code for sparkles in the website
+document.addEventListener(
+  "mousemove",
+  (e) => {
+    if (selectMode) {
+      var arr = [1, 0.5, 0.2]; // stars added
+
+      arr.forEach(function (i) {
+        var x = (1 - i) * 75;
+        var star = document.createElement("div");
+
+        star.className = "star";
+        star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + "px";
+        star.style.left =
+          e.pageX + Math.round(Math.random() * x - x / 2) + "px";
+
+        document.body.appendChild(star);
+
+        window.setTimeout(function () {
+          document.body.removeChild(star);
+        }, Math.round(Math.random() * i * 800)); // lifetime
+      });
+    }
+  },
+  false
+);
